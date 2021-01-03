e33b00d3b980b3845a9b54c7f98cd3543e34a0ab
sahanagana <70218712+sahanagana@users.noreply.github.com>
2021-01-02 18:08:03 -0600

    Create timer.java

---
+++ timer.java
@@ -0,0 +1,16 @@
+public class Countdown{
+
+  private int min;
+  private int seconds;
+  private int sec;
+  public Countdown(minutes){
+    min = minutes;
+    seconds = minutes*60;
+  }
+  public void increment(){
+    var now = new Date();
+  }
+
+
+
+}
